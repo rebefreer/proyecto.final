83a16acf79f86d4e26608a683fb20aa6a3bd633e
lauraberenice <laura.d.mendez@gmail.com>
2019-10-18 17:46:51 -0600

    pagina final

--- main.js
+++ main.js
@@ -1,39 +1,69 @@
-// const words = ["mascotas", "veterinarios", "adopciones"];
-// let i = 0;
-// let timer;
+// Typing effect https://codepen.io/haaswill/pen/VKzXvZ
+const words = ["mascotas", "veterinarios", "alimentos", "perritos", "gatitos"];
+let i = 0;
+let timer;
 
-// function typingEffect() {
-// 	let word = words[i].split("");
-// 	var loopTyping = function() {
-// 		if (word.length > 0) {
-// 			document.getElementById('word').innerHTML += word.shift();
-// 		} else {
-// 			deletingEffect();
-// 			return false;
-// 		};
-// 		timer = setTimeout(loopTyping, 500);
-// 	};
-// 	loopTyping();
-// };
+function typingEffect() {
+	let word = words[i].split("");
+	var loopTyping = function() {
+		if (word.length > 0) {
+			document.getElementById('word').innerHTML += word.shift();
+		} else {
+			deletingEffect();
+			return false;
+		};
+		timer = setTimeout(loopTyping, 200);
+	};
+	loopTyping();
+};
 
-// function deletingEffect() {
-// 	let word = words[i].split("");
-// 	var loopDeleting = function() {
-// 		if (word.length > 0) {
-// 			word.pop();
-// 			document.getElementById('word').innerHTML = word.join("");
-// 		} else {
-// 			if (words.length > (i + 1)) {
-// 				i++;
-// 			} else {
-// 				i = 0;
-// 			};
-// 			typingEffect();
-// 			return false;
-// 		};
-// 		timer = setTimeout(loopDeleting, 200);
-// 	};
-// 	loopDeleting();
-// };
+function deletingEffect() {
+	let word = words[i].split("");
+	var loopDeleting = function() {
+		if (word.length > 0) {
+			word.pop();
+			document.getElementById('word').innerHTML = word.join("");
+		} else {
+			if (words.length > (i + 1)) {
+				i++;
+			} else {
+				i = 0;
+			};
+			typingEffect();
+			return false;
+		};
+		timer = setTimeout(loopDeleting, 200);
+	};
+	loopDeleting();
+};
 
-// typingEffect();
+typingEffect();
+
+// bookmark button https://rudrastyh.com/javascript/favorite-button.html
+function rudr_favorite(a) {
+	pageTitle=homepaws;
+	pageURL=www.homepaws.org;
+	try {
+		// Internet Explorer solution
+		eval("window.external.AddFa-vorite(pageURL, pageTitle)".replace(/-/g,''));
+	}
+	catch (e) {
+		try {
+			// Mozilla Firefox solution
+			window.sidebar.addPanel(pageTitle, pageURL, "");
+		}
+		catch (e) {
+			// Opera solution
+			if (typeof(opera)=="object") {
+				a.rel="sidebar";
+				a.title=pageTitle;
+				a.url=pageURL;
+				return true;
+			} else {
+				// The rest browsers (i.e Chrome, Safari)
+				alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != -1 ? 'Cmd' : 'Ctrl') + '+D to bookmark this page.');
+			}
+		}
+	}
+	return false;
+}
